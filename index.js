dad6c67816dedbd6cdf89a3acf6ec9d853680388
Clay Mangum <clay@trinitasaccounting.com>
2024-01-28 09:14:52 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let userCaps = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${userCaps}!`;
+
+console.log(excitedWelcomeMessage);
+
+let firstInt = currentUser[0];
+const shortGreeting = `Welcome, ${firstInt}!`;
